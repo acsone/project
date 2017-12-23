aa637da0db83e61d419c317a87f90618527eff46
Duc Dao <ducdd@komit-consulting.com>
2017-12-24 01:15:25 +0700

    [MIG] project_task_default_stage: Migration to 11.0

--- project_task_default_stage/models/project.py
+++ project_task_default_stage/models/project.py
@@ -1,9 +1,7 @@
-# -*- coding: utf-8 -*-
 # (c) 2015 Incaser Informatica S.L. - Sergio Teruel
 # (c) 2015 Incaser Informatica S.L. - Carlos Dauden
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
-
-from odoo import models, fields
+from odoo import fields, models
 
 
 class ProjectProject(models.Model):
@@ -14,8 +12,4 @@ class ProjectProject(models.Model):
             ('case_default', '=', True)])
         return ids
 
-    type_ids = fields.Many2many(
-        comodel_name='project.task.type', relation='project_task_type_rel',
-        column1='project_id', column2='type_id', string='Tasks Stages',
-        default=_get_type_common
-    )
+    type_ids = fields.Many2many(default=_get_type_common)
